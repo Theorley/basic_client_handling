7929926c5568cc33576605a799e573d911e6f3d9
braiek <braiekrayen5@gmail.com>
2023-08-18 11:42:52 +0100

    wiw

--- src/main/java/com/example/Billcom4/controller/ContractController.java
+++ src/main/java/com/example/Billcom4/controller/ContractController.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public class ContractController {
     }
     @Autowired
     public ContractRepository contractRepository;
-    @PostMapping(path="/action2")
+    @PutMapping(path="/action2")
     public  @ResponseBody String updateContract(@RequestParam ("clientId") int clientId){
         String output = contractService.assignContract (clientId);
         return "assignmentGreat";
