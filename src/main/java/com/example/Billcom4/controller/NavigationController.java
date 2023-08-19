315211e5e8ae226cb9e67f22ce6ec6163c04d85d
braiek <braiekrayen5@gmail.com>
2023-08-19 15:09:46 +0100

    wiw

--- src/main/java/com/example/Billcom4/controller/NavigationController.java
+++ src/main/java/com/example/Billcom4/controller/NavigationController.java
@@ -2,22 +2,26 @@ package com.example.Billcom4.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
 
     @GetMapping(path = "/addClientPage")
-    public String changeToAddClient (){
+    public String changeToAddClient (Model model){
+        model.addAttribute("activePage", "addClientPage");
         return "home";
     }
-    @GetMapping(path = "/editClientPage")
-    public String changeToEditClient (){
-        return "home";
+    @GetMapping(path = "/showClientPage")
+    public String changeToEditClient (Model model){
+        model.addAttribute("activePage", "showClientPage");
+        return "showClients";
     }
     @GetMapping(path = "/deleteClientPage")
-    public String changeToDeleteClient (){
-        return "home";
+    public String changeToDeleteClient (Model model){
+        model.addAttribute("activePage", "deleteClientPage");
+        return "deleteClient";
     }
 }
 
